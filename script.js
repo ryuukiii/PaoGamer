f9bcd749c16c614b6279a6542fcdfe9f7834f037
Renan Ryuki Kohatsu <renanryuki999@gmail.com>
2023-07-18 01:17:23 -0300

    veryCria

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["mono chamber tiltado", "japa fuzilador", "cria"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
